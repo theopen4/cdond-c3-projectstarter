c51899b889f3344bfd7f6c4eebba8e040516865d
theopen4 <kielze86@gmail.com>
2023-02-17 14:06:32 +0100

    update4 script workflow backend  script2

--- buildBackend.sh
+++ buildBackend.sh
@@ -2,3 +2,7 @@ cd backend
 echo "${{ secrets.NPM_PASSWORD }}" | npm login -u "${{ secrets.NPM_USERNAME }}" --password-stdin
 npm install
 npm run build
+npm run test
+npm run test:e2e
+npm run test:cov
+
