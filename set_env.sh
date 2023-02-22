1d46a32bac0eed9f677c8c6e0a2f52107dea7339
theopen4 <kielze86@gmail.com>
2023-02-22 12:29:37 +0100

    add environement variable

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=ezekiel
+export POSTGRES_PASSWORD=ezekiel1234
+export POSTGRES_HOST=udagram-instance.cjylqncvtcdm.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=udagramdb
+export AWS_BUCKET=ezeudabucket
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
