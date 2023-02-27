30486c561cd180c4e72ffd23961a781f51bdff99
theopen4 <kielze86@gmail.com>
2023-02-27 19:40:51 +0100

    udapte 7 docker compose and files dockers

--- deploymentScript.sh
+++ deploymentScript.sh
@@ -1,7 +1,6 @@
 kubectl  delete deploy backend
 kubectl  delete deploy frontend-deploy
-kubectl  delete deploy reverseproxy
+
 
 kubectl  apply -f backend-deployment.yml
 kubectl  apply -f frontend-deployment.yml
-kubectl  apply -f reverseproxy-deployment.yml
--- set_env.sh
+++ set_env.sh
@@ -8,4 +8,4 @@ export AWS_BUCKET=ezeudabucket
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=testing
-export URL=http://localhost:8100
+export URL=http://localhost:3030
